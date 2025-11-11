475b185c885d65e1d052eec9ffa88d48cf1ccd5b
Mgdadali <109249013+Mgdadali@users.noreply.github.com>
2025-11-11 18:05:30 +0200

    Delete app/add_files.py

--- app/add_files.py
+++
@@ -1,9 +0,0 @@
-from app import crud, db
-
-db.init_db()
-
-crud.add_material("تشريح", "pdf", "BAACAgQAAxkBAAECzFRpE0WDefraOSSkW4uh2omrvf-aVQACUxcAAlkmmVM9LgefiEH1ZzYE")
-crud.add_material("فسيولوجي", "video", "BQACAgQAAxkBA...")
-crud.add_material("تشريح", "reference", "BQACAgQAAxkBA...")
-
-print("✅ تم إضافة الملفات بنجاح!")
